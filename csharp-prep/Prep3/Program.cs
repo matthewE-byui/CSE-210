f11a9e1225342c939af53ca82b5bb9d6dcb7f0e0
Matthew Edgar <edgarmatt26@byui.edu>
2025-05-06 15:13:33 -0600

    resubmitted

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,96 @@
 using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello World! 2");
+//         Console.WriteLine("hey YO!");
+//     }
+// }
+
+// while loops v
+
+// string response = "yes";
+// while (response == "yes")
+// {
+//     Console.Write("Do you want to continue?");
+//     response = Console.ReadLine();
+// }
+
+// Do while loops v
+
+// String response;
+
+// do 
+// {
+//     Console.Write("Do you want to continue? ");
+//     response = Console.ReadLine();
+
+// }while (response == "yes");
+
+// for loops v
+
+// for (int i = 2; i < 20; i++)
+// {
+//     Console.Write(i);
+// }
+
+// foreach loops v
+
+// foreach (string color in color)
+// {
+//     Console.WriteLine(color);
+// }
+
+// Random number generation v
+
+// Random randomGenerator == new Random();
+// int number = randomGenerator.Next(1,11);
+
+// loop program
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100...");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} tries!");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;  // Needed for Sum(), Max(), Average()
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+        // Stretch
+        List<int> positiveNumbers = numbers.Where(n => n > 0).ToList();
+        if (positiveNumbers.Count > 0)
+        {
+            int minPositive = positiveNumbers.Min();
+            Console.WriteLine($"The smallest positive number is: {minPositive}");
+        }
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int n in numbers)
+        {
+            Console.WriteLine(n);
+        }
     }
 }
